1422adad357d0d0c7473b803e24a9016dc183af2
lajawanti <myemail>
2019-08-27 16:34:20 -0400

    Implemented queue

--- Queue_using_stack.py
+++ Queue_using_stack.py
@@ -26,3 +26,33 @@ s1.push(20)
 print("It's Stack data :  ", end = " ")
 s1.print_stack()
 
+#Implement Queue using two Stack instances
+class Queue:
+    def __init__(self):
+        self.s1 = Stack()
+        self.s2 = Stack()
+    
+    def enqueue(self, data):
+        self.s1.push(data)
+    
+    def dequeue(self):
+        while self.s1.length() is not 1:
+            self.s2.push(self.s1.pop())
+        
+        dequeue_data = self.s1.pop()
+        while self.s2.length():
+            self.s1.push(self.s2.pop())
+        
+        return dequeue_data
+     
+    def peek(self):
+        while self.s1.length() is not 1:
+            self.s2.push(self.s1.pop())
+        
+        peek_data = self.s1.pop()
+        self.s2.push(peek_data)
+        while self.s2.length():
+            self.s1.push(self.s2.pop())
+        
+        return peek_data
+
